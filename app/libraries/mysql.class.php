ea8a150da2e0d1808d0cfbd9a44c62a7fa72ec70
Spencer Sharkey <spencer@sf-n.com>
2013-08-13 15:41:28 -0400

    Added Database, SparkSQL, and user shell

--- app/libraries/mysql.class.php
+++ app/libraries/mysql.class.php
@@ -1,7 +1,18 @@
 <?php
 
-class SparkSQL {
+include("zebra_database.class.php");
 
+class SparkSQL extends Zebra_Database {
+
+	function __consturct($host, $user, $password, $database) { 
+		parent::__construct();
+
+		if (ENVIORNMENT == "development") {
+			$this->debug = true;
+		}
+
+		$this->connect($host, $user, $password, $database);
+	}
 
 }
 
---
+++ app/libraries/user.class.php
@@ -0,0 +1,13 @@
+<?php
+
+// We must initialize hte Slidefuse 
+
+class User {
+
+	function __construct($userid = false) {
+		if ($userid) {
+			
+		}
+	}
+
+}
