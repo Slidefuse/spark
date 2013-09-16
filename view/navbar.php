cde5ba10af1e7367a3f60c735892429f61c51024
Spencer Sharkey <spencer@sf-n.com>
2013-09-16 12:59:15 -0400

    Create navbar.php

---
+++ view/navbar.php
@@ -0,0 +1,20 @@
+	<div class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Slidefuse App</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
