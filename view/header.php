497880b50c5745fa3387e490c34ed9391fe2db78
Spencer Sharkey <spencer@sf-n.com>
2013-09-16 12:54:50 -0400

    Create header.php

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title><?= $this->data["pageTitle"]; ?></title>
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+	
+	<? $this->view("navbar"); ?>
+	
